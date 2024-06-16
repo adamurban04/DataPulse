70171c390cb1bcc5c5cd01cec8cd4d7a5a00db2b
Adam Urban <99017970+adamurban04@users.noreply.github.com>
2024-06-17 00:21:54 +0100

    finalDemoW3

--- Research/App.R
+++ Research/App.R
@@ -12,12 +12,15 @@ library(jpeg)  # For handling JPG images
 
 # Read the TSV/CSV file for Datasets
 
-# https://www.cbioportal.org/study/summary?id=luad_tcga_pan_can_atlas_2018
+# https://www.cbioportal.org/study/clinicalData?id=luad_tcga_pan_can_atlas_2018
 luad_data1 <- read_tsv("www/luad_tcga_pan_can_atlas_2018_clinical_data.tsv")
 # https://www.cancerimagingarchive.net/collection/nsclc-radiomics/
 nsclc_data2 <- read.csv("www/manifest1603198545583/NSCLC-Radiomics-Lung1.clinical-version3-Oct-2019.csv")
 # https://www.cbioportal.org/study/clinicalData?id=luad_tcga
 luad_data3 <- read_tsv("www/luad_tcga_firehose_legacy_clinical_data.tsv")
+# https://www.cbioportal.org/study/clinicalData?id=luad_oncosg_2020
+luad_data4 <- read_tsv("www/luad_oncosg_2020_clinical_data.tsv")
+
 
 # Helper function to read JPG files
 read_jpg_files <- function(path) {
@@ -80,7 +83,8 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                                       radioButtons(inputId = "data_choice_tabular", label = "Choose Dataset:", 
                                                    choices = list("LUAD TCGA Pan Can Atlas 2018 Clinical Data" = "luad_data1", 
                                                                   "NSCLC-Radiomics Lung1 Clinical Data" = "nsclc_data2",
-                                                                  "LUAD TCGA Firehose Legacy Clinical Data" = "luad_data3"))
+                                                                  "LUAD TCGA Firehose Legacy Clinical Data" = "luad_data3",
+                                                                  "LUAD (OncoSG, Nat Genet 2020)" = "luad_data4"))
                                     ),
                                     # Image
                                     conditionalPanel(
@@ -93,17 +97,17 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                            # Tabular LUAD 1
                            conditionalPanel(
                              condition = "input.data_choice == 'tabular' && input.data_choice_tabular == 'luad_data1'",
-                             h1("LUAD Clinical Data"),
+                             h1("LUAD TCGA Pan Can Atlas 2018 Clinical Data"),
                              div(class = "scrollable-table", DTOutput("data_dsout1")),
-                             h4("Summary of LUAD Clinical Data"),
+                             h4("Summary of LUAD TCGA Pan Can Atlas 2018 Clinical Data"),
                              div(class = "summary-text-box", verbatimTextOutput("data_summary1"))
                            ),
-                           # Tabular NSCLC
+                           # Tabular NSCLC 2
                            conditionalPanel(
                              condition = "input.data_choice == 'tabular' && input.data_choice_tabular == 'nsclc_data2'",
-                             h1("NSCLC Clinical Data"),
+                             h1("NSCLC-Radiomics Lung1 Clinical Data"),
                              div(class = "scrollable-table", DTOutput("data_dsout2")),
-                             h4("Summary of NSCLC Clinical Data"),
+                             h4("Summary of NSCLC-Radiomics Lung1 Clinical Data"),
                              div(class = "summary-text-box", verbatimTextOutput("data_summary2"))
                            ),
                            # Tabular LUAD 3
@@ -113,6 +117,14 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                              div(class = "scrollable-table", DTOutput("data_dsout3")),
                              h4("Summary of LUAD TCGA Firehose Legacy Clinical Data"),
                              div(class = "summary-text-box", verbatimTextOutput("data_summary3"))
+                           ),
+                           # Tabular LUAD 4
+                           conditionalPanel(
+                             condition = "input.data_choice == 'tabular' && input.data_choice_tabular == 'luad_data4'",
+                             h1("LUAD (OncoSG, Nat Genet 2020) Clinical Data"),
+                             div(class = "scrollable-table", DTOutput("data_dsout4")),
+                             h4("Summary of LUAD (OncoSG, Nat Genet 2020) Clinical Data"),
+                             div(class = "summary-text-box", verbatimTextOutput("data_summary4"))
                            ),
                            # Image NSCLC
                            conditionalPanel(
@@ -131,49 +143,48 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                            fluidRow(
                              column(12, class = "compact-container",
                                     div(class = "compact-select",
-                                        selectInput("plot_data_choice", "Choose Data Type:", 
-                                                    choices = c("Tabular Data" = "tabular", "Image Data" = "image"))
+                                        radioButtons(inputId = "plot_data_choice_tabular", label = "Choose Dataset:", 
+                                                     choices = list("LUAD TCGA Pan Can Atlas 2018 Clinical Data" = "luad_data1", 
+                                                                    "NSCLC-Radiomics Lung1 Clinical Data" = "nsclc_data2",
+                                                                    "LUAD TCGA Firehose Legacy Clinical Data" = "luad_data3",
+                                                                    "LUAD (OncoSG, Nat Genet 2020)" = "luad_data4"))
                                     ),
-                                    # Tabular
+
+                                    # LUAD Tabular 1
                                     conditionalPanel(
-                                      condition = "input.plot_data_choice == 'tabular'",
-                                      radioButtons(inputId = "plot_data_choice_tabular", label = "Choose Dataset:", 
-                                                   choices = list("LUAD TCGA Pan Can Atlas 2018 Clinical Data" = "luad_data1", 
-                                                                  "NSCLC-Radiomics-Lung1.clinical-version3-Oct-2019" = "nsclc_data2",
-                                                                  "LUAD TCGA Firehose Legacy Clinical Data" = "luad_data3"))
-                                    ),
-                                    # LUAD Tabular
-                                    conditionalPanel(
-                                      condition = "input.plot_data_choice == 'tabular' && input.plot_data_choice_tabular == 'luad_data1'",
+                                      condition = "input.plot_data_choice_tabular == 'luad_data1'",
                                       radioButtons(inputId = "plot_choice_tabular1", label = "Choose Plot:", 
                                                    choices = list("Age" = "age", 
                                                                   "Sex" = "sex",
                                                                   "Race" = "race",
                                                                   "Ethnicity" = "ethnicity"))
                                     ),
-                                    # NSCLC Tabular
+                                    # NSCLC Tabular 2
                                     conditionalPanel(
-                                      condition = "input.plot_data_choice == 'tabular' && input.plot_data_choice_tabular == 'nsclc_data2'",
+                                      condition = "input.plot_data_choice_tabular == 'nsclc_data2'",
                                       radioButtons(inputId = "plot_choice_tabular2", label = "Choose Plot:", 
                                                    choices = list("Age" = "age2", 
                                                                   "Sex" = "sex2"))
                                     ),
                                     # LUAD Tabular 3
                                     conditionalPanel(
-                                      condition = "input.plot_data_choice == 'tabular' && input.plot_data_choice_tabular == 'luad_data3'",
+                                      condition = "input.plot_data_choice_tabular == 'luad_data3'",
                                       radioButtons(inputId = "plot_choice_tabular3", label = "Choose Plot:", 
                                                    choices = list("Age" = "age3", 
                                                                   "Sex" = "sex3",
                                                                   "Race" = "race3",
                                                                   "Ethnicity" = "ethnicity3"))
-                                    ),    
-                                    
-                                    # Image
-                                    conditionalPanel(
-                                      condition = "input.plot_data_choice == 'image'",
-                                      radioButtons(inputId = "plot_data_choice_image", label = "Choose Dataset:", 
-                                                   choices = list("NSCLC-Radiomics-Lung1.clinical-version3-Oct-2019" = "nsclc_data2"))
                                     ),
+                                    
+                                    # LUAD Tabular 4
+                                    conditionalPanel(
+                                      condition = "input.plot_data_choice_tabular == 'luad_data4'",
+                                      radioButtons(inputId = "plot_choice_tabular4", label = "Choose Plot:", 
+                                                   choices = list("Age" = "age4", 
+                                                                  "Sex" = "sex4",
+                                                                  "Ethnicity" = "ethnicity4"))
+                                    ),  
+                                    
                                     plotOutput(outputId = "dynamic_plot")
                              )
                            )
@@ -185,8 +196,13 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                              column(3, class = "compact-container",
                                     div(class = "compact-select",
                                         selectInput("jpg_file_choice", "Choose JPG File:", 
-                                                    choices = list.files("www/manifest1603198545583/NSCLCRadiomics/one", pattern = "\\.jpg$"))
-                                    )
+                                                    choices = list.files("www/manifest1603198545583/NSCLCRadiomics/images", pattern = "\\.jpg$"))
+                                    ),
+                                    h4(HTML("<u>x.abc.jpg</u>")),
+                                    h5(HTML("<i>x</i> for patient")),
+                                    h5(HTML("<i>abc</i> for picture in sequence"))
+                                    
+                                    
                              ),
                              
                              column(12, class = "image-box",
@@ -226,6 +242,10 @@ server <- function(input, output) {
     datatable(luad_data3, options = list(scrollX = TRUE, lengthMenu = list(c(10, 25, 50, -1), c('10', '25', '50', 'All'))))
   })
   
+  output$data_dsout4 <- renderDataTable({
+    datatable(luad_data4, options = list(scrollX = TRUE, lengthMenu = list(c(10, 25, 50, -1), c('10', '25', '50', 'All'))))
+  })
+  
   # Summaries
   output$data_summary1 <- renderPrint({
     summary(luad_data1)
@@ -239,11 +259,15 @@ server <- function(input, output) {
     summary(luad_data3)
   })
   
+  output$data_summary4 <- renderPrint({
+    summary(luad_data4)
+  })
+  
   # Function to render selected JPG image
   output$jpg_image <- renderPlot({
     req(input$jpg_file_choice)  # Ensure a file is selected
     
-    file_path <- file.path("www/manifest1603198545583/NSCLCRadiomics/one", input$jpg_file_choice)
+    file_path <- file.path("www/manifest1603198545583/NSCLCRadiomics/images", input$jpg_file_choice)
     
     # Use jpeg package to read and display the JPG image
     img <- readJPEG(file_path)
@@ -258,36 +282,43 @@ server <- function(input, output) {
   total_samples_luad <- nrow(luad_data1)
   total_samples_nsclc <- nrow(nsclc_data2)
   total_samples_luad3 <- nrow(luad_data3)
+  total_samples_luad4 <- nrow(luad_data4)
   
   # Reactive expression to filter data based on plot choice
   filtered_data <- reactive({
-    if (input$plot_data_choice == "tabular" && input$plot_data_choice_tabular == "luad_data1") {
+    if (input$plot_data_choice_tabular == "luad_data1") {
       switch(input$plot_choice_tabular1,
              "age" = luad_data1 %>% filter(!is.na(`Diagnosis Age`)),
              "sex" = luad_data1 %>% filter(!is.na(Sex)),
              "race" = luad_data1 %>% filter(!is.na(`Race Category`)),
              "ethnicity" = luad_data1 %>% filter(!is.na(`Ethnicity Category`))
       )
-    } else if (input$plot_data_choice == "tabular" && input$plot_data_choice_tabular == "nsclc_data2") {
+    } else if (input$plot_data_choice_tabular == "nsclc_data2") {
       switch(input$plot_choice_tabular2,
              "age2" = nsclc_data2 %>% filter(!is.na(age)),
              "sex2" = nsclc_data2 %>% filter(!is.na(gender))
       )
-    } else if (input$plot_data_choice == "tabular" && input$plot_data_choice_tabular == "luad_data3") {
+    } else if (input$plot_data_choice_tabular == "luad_data3") {
       switch(input$plot_choice_tabular3,
              "age3" = luad_data3 %>% filter(!is.na(`Diagnosis Age`)),
              "sex3" = luad_data3 %>% filter(!is.na(Sex)),
              "race3" = luad_data3 %>% filter(!is.na(`Race Category`)),
              "ethnicity3" = luad_data3 %>% filter(!is.na(`Ethnicity Category`))
       )
-    }
+    } else if (input$plot_data_choice_tabular == "luad_data4") {
+      switch(input$plot_choice_tabular4,
+             "age4" = luad_data4 %>% filter(!is.na(Age)),
+             "sex4" = luad_data4 %>% filter(!is.na(Sex)),
+             "ethnicity4" = luad_data4 %>% filter(!is.na(`Ethnicity Category`))
+      )}
+    
   })
   
   output$dynamic_plot <- renderPlot({
     # Get the filtered data
     filt_data <- filtered_data()
     
-    if (input$plot_data_choice == "tabular" && input$plot_data_choice_tabular == "luad_data1") {
+    if (input$plot_data_choice_tabular == "luad_data1") {
       total_filtered_samples <- nrow(filt_data)
       
       plot <- switch(input$plot_choice_tabular1,
@@ -336,7 +367,7 @@ server <- function(input, output) {
       
       print(plot)
       
-    } else if (input$plot_data_choice == "tabular" && input$plot_data_choice_tabular == "nsclc_data2") {
+    } else if (input$plot_data_choice_tabular == "nsclc_data2") {
       total_filtered_samples <- nrow(filt_data)
       
       plot <- switch(input$plot_choice_tabular2,
@@ -362,7 +393,7 @@ server <- function(input, output) {
       )
       
       print(plot)
-    } else if (input$plot_data_choice == "tabular" && input$plot_data_choice_tabular == "luad_data3") {
+    } else if (input$plot_data_choice_tabular == "luad_data3") {
       total_filtered_samples <- nrow(filt_data)
       
       plot <- switch(input$plot_choice_tabular3,
@@ -409,6 +440,44 @@ server <- function(input, output) {
                      }
       )
       
+      print(plot)
+      
+    } else if (input$plot_data_choice_tabular == "luad_data4") {
+      total_filtered_samples <- nrow(filt_data)
+      
+      plot <- switch(input$plot_choice_tabular4,
+                     "age4" = {
+                       average_age <- mean(filt_data$Age, na.rm = TRUE)
+                       ggplot(filt_data, aes(x = Age)) +
+                         geom_bar() +
+                         labs(title = "Distribution of Diagnosis Age", x = "Age", y = "Count") +
+                         annotate("text", label = paste("Valid Filtered Samples:", total_filtered_samples, "NA:", (total_samples_luad3 - total_filtered_samples)), 
+                                  x = Inf, y = Inf, hjust = 1.0, vjust = 1.5) +
+                         annotate("text", label = paste("Average Diagnosis Age:", round(average_age, 2)), 
+                                  x = Inf, y = Inf, hjust = 1.1, vjust = 3.8)
+                     },
+                     "sex4" = {
+                       sex_counts <- filt_data %>% count(Sex)
+                       ggplot(sex_counts, aes(x = "", y = n, fill = Sex)) +
+                         geom_bar(stat = "identity", width = 1) +
+                         coord_polar("y", start = 0) +
+                         theme_void() +
+                         labs(title = "Male/Female Diagnosed Ratio") +
+                         scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+                         geom_text(aes(label = n), position = position_stack(vjust = 0.5)) +
+                         annotate("text", x = Inf, y = Inf, label = paste("Valid Filtered Samples:", total_filtered_samples, "NA:", (total_samples_luad - total_filtered_samples)), hjust = 0.5, vjust = -1)
+                     },
+                     "ethnicity4" = {
+                       ethnicity_counts <- filt_data %>% count(`Ethnicity Category`)
+                       ggplot(ethnicity_counts, aes(x = "", y = n, fill = `Ethnicity Category`)) +
+                         geom_bar(stat = "identity", width = 1) +
+                         coord_polar("y", start = 0) +
+                         theme_void() +
+                         labs(title = "Ethnicity Category Diagnosed Ratio") +
+                         geom_text(aes(label = n), position = position_stack(vjust = 0.5)) +
+                         annotate("text", x = Inf, y = Inf, label = paste("Valid Filtered Samples:", total_filtered_samples, "NA:", (total_samples_luad3 - total_filtered_samples)),  hjust = 0.5, vjust = -1)
+                     }
+      )
       print(plot)
     }
   })
