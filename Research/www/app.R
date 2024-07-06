2dba175d3f6c78f296a7dad944c8e55c754e912f
Adam Urban <adamurban73@gmail.com>
2024-07-06 12:17:57 +0200

    Revert "codeSplitted"
    This reverts commit a6a15dbf0ebf11cb41bcb98b8e714b302ad17779.

--- Research/www/app.R
+++
@@ -1,7 +0,0 @@
-source("global.R") # Global Dependencies (packages)
-source("data_loading.R") # Loading Datasets
-source("helper_functions.R") # Helper Functions for handling DICOM files
-source("ui.R") # User Interface
-source("server.R") # Server function
-
-shinyApp(ui = ui, server = server)
